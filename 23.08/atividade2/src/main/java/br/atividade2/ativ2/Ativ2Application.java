2057f2f43725859c5daefc8f69e3d8abb2ab9187
weslley silva <weslleysilvapaiva@outlook.com>
2021-08-31 09:43:45 -0300

    CRUD 30.08

---
+++ 23.08/atividade2/src/main/java/br/atividade2/ativ2/Ativ2Application.java
@@ -0,0 +1,13 @@
+package br.atividade2.ativ2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ativ2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ativ2Application.class, args);
+	}
+
+}
---
+++ 23.08/atividade2/src/test/java/br/atividade2/ativ2/Ativ2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.atividade2.ativ2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ativ2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
